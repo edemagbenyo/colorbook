618ad71b2add86d0fed2e3ca757c228f8e000e5e
Edemone <edem.agbenyo@gmail.com>
2020-03-02 07:31:40 +0000

    Implement drawing on canvas

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+let board = document.querySelector("#board")
+board.width = ((window.innerWidth)*70)/100;
+board.height = window.innerHeight;
+const ctx = board.getContext('2d');
+
+ctx.strokeStyle = "#BADA55"
+ctx.lineJoin="round"
+ctx.lineCap="round"
+ctx.lineWidth=10
+
+let isDrawing = false;
+let lastX = 0
+let lastY = 0 
+let hue = 0
+
+const draw = (e)=>{
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+  ctx.beginPath();
+
+  //start from
+  ctx.moveTo(lastX, lastY)
+
+  //go to
+  ctx.lineTo(lastX, lastY)
+  ctx.stroke()
+  // console.log(e.offsetY,e.offsetX);
+  lastX = e.offsetX
+  lastY = e.offsetY
+  
+  // [lastX, lastY] = [e.offsetX, e.offsetY]
+  hue++;
+}
+
+board.addEventListener("mousemove",draw)
+board.addEventListener("mousedown",(e)=>{
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+})
+board.addEventListener("mouseup",()=>{
+  isDrawing = false;
+})
+board.addEventListener("mouseout",()=>{
+  isDrawing = false;
+})
